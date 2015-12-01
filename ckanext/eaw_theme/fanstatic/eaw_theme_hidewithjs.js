81b21f82073d627812676faebe7c256d97882485
Harald von Waldow <harald.vonwaldow@eawag.ch>
2015-12-01 17:13:16 +0100

    advanced search done

--- ckanext/eaw_theme/fanstatic/eaw_theme_hidewithjs.js
+++ ckanext/eaw_theme/fanstatic/eaw_theme_hidewithjs.js
@@ -1,13 +1,21 @@
+// Module to be bound to the "Advanced Search Button".
+// Makes the button visible, implements toggling of
+// the next element that contains helptext and searchbox,
+// which it also hides.
+
 "use strict"
 
 ckan.module('eaw_theme_hidewithjs', function($, _) {
     return {
 	initialize: function () {
-	    // alert("initialized hidewithjs");
-	    console.log("initialized hidewithjs", this.el);
+	    var _this = this.el;
 	    this.el.show();
 	    this.el.next().hide();
-	    // this.el.style.color = "red";
+	    this.el.next().find( 'h4' ).hide();
+	    this.el.click(function () {
+		_this.next().toggle();
+		return(false);
+	    });
 	}
     };
 });
