9194fb0780d4c061ea0cfda3b10d0ed323a8aca4
Harald von Waldow <harald.vonwaldow@eawag.ch>
2016-01-12 17:29:30 +0100

    finished Dataset -> Package

--- ckanext/eaw_theme/plugin.py
+++ ckanext/eaw_theme/plugin.py
@@ -1,12 +1,13 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as tk
+from ckan.lib.plugins import DefaultTranslation
 
 
-class Eaw_ThemePlugin(plugins.SingletonPlugin):
+class Eaw_ThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
+    plugins.implements(plugins.ITranslation)
     plugins.implements(plugins.IConfigurer)
 
     # IConfigurer
-
     def update_config(self, config_):
         tk.add_template_directory(config_, 'templates')
         tk.add_public_directory(config_, 'public')
