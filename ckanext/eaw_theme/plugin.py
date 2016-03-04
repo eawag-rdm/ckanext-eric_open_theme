d33f25e3beaf1dc7278608143b37f2c10e1354eb
Harald von Waldow <harald.vonwaldow@eawag.ch>
2016-03-04 23:58:47 +0100

    fixed facet list

--- ckanext/eaw_theme/plugin.py
+++ ckanext/eaw_theme/plugin.py
@@ -1,11 +1,22 @@
+from collections import OrderedDict
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as tk
 from ckan.lib.plugins import DefaultTranslation
 
 
+# This is a workaround issue  #2713
+# https://github.com/ckan/ckan/issues/2713
+def new_facet_dict(facet_dict, new_facets):
+    for e in facet_dict:
+        del facet_dict[e]
+    for k, v in new_facets:
+        facet_dict[k] = v
+    return facet_dict
+
 class Eaw_ThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.ITranslation)
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets)
 
     # IConfigurer
     def update_config(self, config_):
@@ -13,4 +24,28 @@ class Eaw_ThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
         tk.add_public_directory(config_, 'public')
         tk.add_resource('fanstatic/vendor/bootstrap-switch', 'bootstrap-switch')
         tk.add_resource('fanstatic', 'eaw_theme')
+
+    # IFacets
+    def dataset_facets(self, facet_dict, package_type):
+         new_facets = [('organization', u'Organizations'),
+                       ('groups', u'Projects'),
+                       ('tags', u'Keywords'),
+                       ('vocab_variables', u'Variables'),
+                       ('vocab_systems', u'Systems')]
+         return new_facet_dict(facet_dict, new_facets)
+
+    def group_facets(self, facet_dict, group_type, package_type):
+        new_facets =  [('organization', u'Organizations'),
+                       ('tags', u'Keywords'),
+                       ('vocab_variables', u'Variables'),
+                       ('vocab_systems', u'Systems')]
+        return new_facet_dict(facet_dict, new_facets)
+
+    def organization_facets(self, facet_dict, organization_type, package_type):
+        new_facets =  [('organization', u'Organizations'),
+                       ('tags', u'Keywords'),
+                       ('vocab_variables', u'Variables'),
+                       ('vocab_systems', u'Systems')]
+        return new_facet_dict(facet_dict, new_facets)
+    
 
