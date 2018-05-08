20c156f32b8cb32ae5fd14bd598ce779a46dcfe7
Harald von Waldow <harald.vonwaldow@eawag.ch>
2018-05-08 21:06:24 +0200

    removed debug - log

--- ckanext/eaw_theme/fanstatic/clickwholedivs.js
+++ ckanext/eaw_theme/fanstatic/clickwholedivs.js
@@ -1,6 +1,5 @@
 $("li.resource-item").click(function(e) {
     var url = $(this).children("a.heading").first().attr("href");
-    console.log(url);
     if ($(e.target).is("li.resource-item")) {
         window.location = url;
     }
